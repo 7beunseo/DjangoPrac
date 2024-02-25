c256b29573812e82f42094d5f9f9ee01e04c7f3c
7beunseo <kimes0403@gmail.com>
2024-02-26 04:34:47 +0900

    ✨ 일대다 관계의 일에서 다 조회

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('', mypage, name = "mypage"),
     path('my-likes/', my_likes, name = "my-likes"),
+    path('my-posts/', my_posts, name = "my-posts")
 ]
--- users/views.py
+++ users/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 from posts.models import Post
 from .models import User
 
+# mypage
 def mypage(request):
     return render(request, 'mypage.html')
 
+# my likes at posts
 def my_likes(request):
     posts = request.user.post_like.all() # 역참조
-    print(posts)
     return render(request, 'my_likes.html', {'posts':posts})
+
+# my posts
+def my_posts(request):
+    posts = Post.objects.filter(writer = request.user)
+    return render(request, 'my_posts.html', {'posts':posts})
